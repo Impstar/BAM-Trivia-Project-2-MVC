e15351b6003edac3a56e4d197a6fa8f2c0e9ff23
Your Name <mpkagel@gmail.com>
2019-03-26 12:53:54 -0500

    Added Quizzes to the nav bar, added get all quizzes to Quizzes/Index

--- BAMTriviaProject2MVC/BAMTriviaProject2MVC/ApiModels/ApiQuizzes.cs
+++ BAMTriviaProject2MVC/BAMTriviaProject2MVC/ApiModels/ApiQuizzes.cs
@@ -12,11 +12,11 @@ namespace BAMTriviaProject2MVC.ApiModels
         public int Difficulty { get; set; }
         public string Category { get; set; }
 
-        public List<string> Categories = new List<string>()
-        {
-            "QC",
-            "Beer",
-            "Movies"
-        };
+        //public List<string> Categories = new List<string>()
+        //{
+        //    "QC",
+        //    "Beer",
+        //    "Movies"
+        //};
     }
 }
